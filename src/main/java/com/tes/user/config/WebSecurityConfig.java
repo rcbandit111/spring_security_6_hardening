b75a8e9076254e8dc145861284c66bc7db1cfacc
Plamen Terziev <pterziev@datexcorp.com>
2023-05-28 17:58:21 +0300

    Updated code

--- src/main/java/com/tes/user/config/WebSecurityConfig.java
+++ src/main/java/com/tes/user/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.tes.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
     @Bean
