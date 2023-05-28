4998d0776e4f70af9f0d1146622d7269396e0c36
Plamen Terziev <pterziev@datexcorp.com>
2023-05-28 16:37:44 +0300

    Updated code

--- src/main/java/com/tes/user/config/WebSecurityConfig.java
+++ src/main/java/com/tes/user/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,8 +33,16 @@ public class WebSecurityConfig {
                             .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
                             .collect(Collectors.toList());
                     return new JwtAuthenticationToken(jwt, grantedAuthorities);
-                })))
-        ;
+                })));
+
+        // State-less session (state in access-token only)
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Disable CSRF because of state-less session-management
+        httpSecurity.csrf().disable();
+
+        // Disable anonymous
+        httpSecurity.anonymous().disable();
 
         return httpSecurity.build();
     }
